59ea31704a53f0c2087f8da9e0bba1b5c17a801c
naens <naens@gmx.com>
2018-03-13 23:17:00 +0100

    java:JackCompiler: add SymbolTable and VMWriter classes

---
+++ java/JackCompiler/SymbolTable.java
@@ -0,0 +1,28 @@
+class SymbolTable {
+
+    public enum Kind {FIELD, STATIC, LOCAL}
+
+    public SymbolTable() {
+    }
+
+    public void startSubroutine() {
+    }
+
+    public void define() {
+    }
+
+    public void varCount() {
+    }
+
+    public Kind kindOf(String s) {
+        return null;
+    }
+
+    public String typeOf(String s) {
+        return null;
+    }
+
+    public int indexOf(String s) {
+        return -1234;
+    }
+}
---
+++ java/JackCompiler/VMWriter.java
@@ -0,0 +1,17 @@
+class VMWriter {
+
+    public VMWriter() {
+    }
+
+    public void writePush() {
+    }
+
+    public void writePop() {
+    }
+
+    public void writeArithmetic() {
+    }
+
+    /* TODO */
+
+}
