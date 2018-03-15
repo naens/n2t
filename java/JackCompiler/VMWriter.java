9cfc0c5cd7f617970a48bd58299b23e6f6017c15
naens <naens@gmx.com>
2018-03-15 11:14:22 +0100

    java:JackCompiler: implement VMWriter methods

--- java/JackCompiler/VMWriter.java
+++ java/JackCompiler/VMWriter.java
@@ -13,24 +13,31 @@ class VMWriter {
     }
 
     public void writePush(Segment segment, int index) {
+        ps.println(String.format("push %s %d", segment.toString(), index));
     }
 
     public void writePop(Segment segment, int index) {
+        ps.println(String.format("pop %s %d", segment.toString(), index));
     }
 
     public void writeArithmetic(Command command) {
+        ps.println(command.toString());
     }
 
     public void writeLabel(String label) {
+        ps.println(String.format("label %s", label));
     }
 
     public void writeGoto(String label) {
+        ps.println(String.format("goto %s", label));
     }
 
     public void writeIf(String label) {
+        ps.println(String.format("if-goto %s", label));
     }
 
     public void writeCall(String name, int nArgs) {
+        ps.println(String.format("call %s %d", name, nArgs));
     }
 
     public void writReturn() {
@@ -39,6 +46,7 @@ class VMWriter {
 
     /* closes the output file */
     public void close() {
+        ps.close();
     }
 
 }
